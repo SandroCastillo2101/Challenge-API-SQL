c0f36d754c0931401c8eca8e140f703ef1778c0b
SandroCastillo <sandro.castillo.a@uni.pe>
2025-03-08 22:37:28 -0500

    v0.3.0 CSV file sent

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Challenge-API-SQL
 
+[v0.3.0] - CSV file sent
+
+In this version, a function to receive a CSV file and save it is created.
+
 [v0.2.0] - Flask introduced
 
 In this version, Flask is introduced and endpoints are created.
--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from modules.transform.Transformation import employees_by_quarter, hired_employees_by_department
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 main_path = "resources/data/input"
+
+
+@app.route("/upload_csv", methods=["POST"])
+def upload_csv():
+    if "file" not in request.files:
+        return jsonify({"error": "No se encontró el archivo"}), 400
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "Nombre de archivo no válido"}), 400
+
+    if file.filename == "departments.csv":
+        filepath = os.path.join(main_path, "departments", file.filename)
+    elif file.filename == "hired_employees.csv":
+        filepath = os.path.join(main_path, "employees", file.filename)
+    elif file.filename == "Jobs.csv":
+        filepath = os.path.join(main_path, "jobs", file.filename)
+
+    file.save(filepath)
+
+    return jsonify({"message": f"Archivo guardado en {filepath}"}), 200
 
 
 @app.route("/employees_by_quarter", methods=["GET"])
